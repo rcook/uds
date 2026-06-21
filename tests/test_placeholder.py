2dd7e46d4c165fe5ef3ee840d61f342b716609ed
Richard Cook <rcook@rcook.org>
2026-06-21 00:22:29 -0700

    Remove brittle test

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -2,12 +2,7 @@ from __future__ import annotations
 
 from click.testing import CliRunner
 
-from bygge import VERSION
 from bygge.main import main
-
-
-def test_version() -> None:
-    assert VERSION == "0.1.0"
 
 
 def test_main_runs() -> None:
