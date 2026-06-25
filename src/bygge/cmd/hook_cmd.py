14ab62c6db51ea66346d897c379eddc573faf92f
Richard Cook <rcook@rcook.org>
2026-06-24 23:05:38 -0700

    Create hook directory as needed

--- src/bygge/cmd/hook_cmd.py
+++ src/bygge/cmd/hook_cmd.py
@@ -16,25 +16,33 @@ _PRE_COMMIT_HOOK_TEMPLATE: Path = (
 
 
 def hook(workspace: Workspace, dir: Path | None, create_pre_commit: bool) -> None:
-    local_dir = workspace.workspace_dir / "hooks" if dir is None else dir
+    hook_dir = workspace.workspace_dir / "hooks" if dir is None else dir
 
     try:
-        repo_path = local_dir.relative_to(workspace.workspace_dir).as_posix()
+        hook_rel_path = hook_dir.relative_to(workspace.workspace_dir)
     except ValueError as e:  # pragma: no cover
-        raise ByggeError(f"{local_dir} is not in subpath of {workspace.workspace_dir}") from e
+        raise ByggeError(f"{hook_dir} is not in subpath of {workspace.workspace_dir}") from e
 
     _ = run_subprocess(
-        cmd=["git", "config", "core.hooksPath", repo_path],
+        cmd=["git", "config", "core.hooksPath", hook_rel_path.as_posix()],
         cwd=workspace.workspace_dir,
         check=True,
         yes=True,
     )
-    click.echo(f"Git hooks installed into {repo_path} directory")
+    click.echo(f"Git hooks installed into {hook_dir} directory")
+
+    if not hook_dir.is_dir() and not create_pre_commit:  # pragma: no cover
+        click.echo(
+            f"WARNING: Directory {hook_dir} does not exist yet: "
+            + "pass --create-pre-commit to create directory with pre-commit hook"
+        )
 
     if create_pre_commit:
         venv_rel_path = workspace.venv_dir.relative_to(workspace.workspace_dir).as_posix()
 
-        pre_commit_path = local_dir / "pre-commit"
+        hook_dir.mkdir(parents=True, exist_ok=True)
+
+        pre_commit_path = hook_dir / "pre-commit"
         if pre_commit_path.is_file():
             raise ByggeError(f"Pre-commit hook file {pre_commit_path} already exists")
 
